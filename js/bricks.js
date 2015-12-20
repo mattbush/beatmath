8883b66f70e78523c20f1101e35497a8c20fd7a4
mattbush <matt.s.bush@gmail.com>
2015-12-20 03:45:28 -0800

    add constraints to make it impossible for holes to appear

--- js/bricks.js
+++ js/bricks.js
@@ -5,7 +5,7 @@ var MinHeap = require('min-heap');
 
 const {WIDTH_PX, HEIGHT_PX} = require('./beatmath_constants.js');
 
-const REFRESH_RATE = 50;
+const REFRESH_RATE = 20;
 
 const INV_SQRT_3 = 1 / Math.sqrt(3);
 const TWO_X_INV_SQRT_3 = 2 / Math.sqrt(3);
@@ -23,11 +23,23 @@ const NEIGHBOR_OFFSETS_AND_POSSIBLE_ORIENTATIONS_BY_PARITY = [
         {x: 2, y: 0, constraints: {1: [0], 3: [2, 4], 5: [2, 4]}},
         {x: -1, y: 1, constraints: {5: [4], 1: [0, 2], 3: [0, 2]}},
         {x: -1, y: -1, constraints: {3: [2], 1: [0, 4], 5: [0, 4]}},
+        {x: 3, y: 1, constraints: {2: [2, 4]}},
+        {x: 0, y: 2, constraints: {2: [0, 2]}},
+        {x: -3, y: 1, constraints: {0: [0, 2]}},
+        {x: -3, y: -1, constraints: {0: [0, 4]}},
+        {x: 0, y: -2, constraints: {4: [0, 4]}},
+        {x: 3, y: -1, constraints: {4: [2, 4]}},
     ],
     [
         {x: -2, y: 0, constraints: {0: [1], 2: [3, 5], 4: [3, 5]}},
         {x: 1, y: 1, constraints: {2: [3], 0: [1, 5], 4: [1, 5]}},
         {x: 1, y: -1, constraints: {4: [5], 0: [1, 3], 2: [1, 3]}},
+        {x: 3, y: 1, constraints: {1: [1, 5]}},
+        {x: 0, y: 2, constraints: {5: [1, 5]}},
+        {x: -3, y: 1, constraints: {5: [3, 5]}},
+        {x: -3, y: -1, constraints: {3: [3, 5]}},
+        {x: 0, y: -2, constraints: {3: [1, 3]}},
+        {x: 3, y: -1, constraints: {1: [1, 3]}},
     ],
 ];
 const FILLS = ['#fd0', '#fd0', '#f00', '#f00', '#0e0', '#0e0'];
@@ -58,12 +70,13 @@ var insertItemIntoHeap = function(heap, item) {
 };
 
 var getPossibleOrientationsBasedOnNeighbors = function(grid, newItem) {
-    var possibleOrientations = POSSIBLE_ORIENTATIONS_BY_PARITY[newItem.parity];
+    var allPossibleOrientations = POSSIBLE_ORIENTATIONS_BY_PARITY[newItem.parity];
+    var possibleOrientations = allPossibleOrientations;
     for (var neighborOffset of NEIGHBOR_OFFSETS_AND_POSSIBLE_ORIENTATIONS_BY_PARITY[newItem.parity]) {
         var neighborCoords = `${newItem.x + neighborOffset.x},${newItem.y + neighborOffset.y}`;
         if (_.has(grid, neighborCoords)) {
             var neighborOrientation = grid[neighborCoords];
-            possibleOrientations = _.intersection(possibleOrientations, neighborOffset.constraints[neighborOrientation]);
+            possibleOrientations = _.intersection(possibleOrientations, neighborOffset.constraints[neighborOrientation] || allPossibleOrientations);
         }
     }
     return possibleOrientations;
@@ -117,7 +130,7 @@ var BrickGrid = React.createClass({
 
         return (
             <svg width={WIDTH_PX} height={HEIGHT_PX} className="brickGrid">
-                <g transform={`translate(${WIDTH_PX / 2}, ${HEIGHT_PX / 2}) scale(25)`}>
+                <g transform={`translate(${WIDTH_PX / 2}, ${HEIGHT_PX / 2}) scale(20)`}>
                     {triangles}
                 </g>
             </svg>
