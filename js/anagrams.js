96b67ff81726b0171bd90331b75ef94912b17d28
mattbush <matt.s.bush@gmail.com>
2015-12-26 11:13:19 -0800

    dancingtext class

--- js/anagrams.js
+++ js/anagrams.js
@@ -15,6 +15,7 @@ const JUMP_UNIT_HEIGHT = -16;
 const ANAGRAM_SET_CYCLE_TIME = 15000;
 const ANAGRAM_CYCLE_TIME = 3000;
 const LETTER_TRANSITION_TIME = 1000;
+const WORD_TRANSITION_TIME = 600;
 const LETTER_TILT_TIME = 600;
 
 var colorCache = {};
@@ -127,6 +128,39 @@ var RearrangingLetter = React.createClass({
             </g>
         );
     },
+});
+
+var DancingText = React.createClass({
+    render: function() {
+        var textLength = this.props.text.length;
+        var letterSpacing = Math.min(LETTER_SPACING, (WIDTH_PX - LETTER_SPACING) / textLength);
+        var xOffset = -1 * (textLength - 1) / 2 * letterSpacing;
+
+        var letterComponents = [];
+        for (var letterIndex = 0; letterIndex < textLength; letterIndex++) {
+            let x = letterIndex * letterSpacing;
+            style = {
+                transform: `translate(${x}px, 0px)`,
+            };
+            letterComponents.push(
+                <g style={style} key={letterIndex}>
+                    <Letter character={this.props.text[letterIndex]} index={letterIndex} />
+                </g>
+            );
+        }
+
+        var style = {
+            transform: `translate(${xOffset}px, 0px)`,
+            transition: `transform ${WORD_TRANSITION_TIME / 2000}s ease`,
+        };
+
+        return (
+            <g style={style}>
+                {letterComponents}
+            </g>
+        );
+
+    }
 });
 
 var AnagramSetComponent = React.createClass({
