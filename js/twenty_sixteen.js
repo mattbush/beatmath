1d8b53c0193ee2dd6df0ecf16031930d59bc99a6
mattbush <matt.s.bush@gmail.com>
2016-01-06 23:42:02 -0800

    twenty sixteen start

---
+++ js/twenty_sixteen.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+      <div className="main">
+      </div>,
+      document.getElementById('start')
+    );
+});
