7d2cd16cacec37f5da7ac91457696f3336d041b1
mattbush <matt.s.bush@gmail.com>
2016-01-13 01:16:03 -0800

    LatticePixel in its own file

--- js/components/lattice/LatticeGrid.js
+++ js/components/lattice/LatticeGrid.js
@@ -1,47 +1,11 @@
 var _ = require('underscore');
 var React = require('react');
-var tinycolor = require('tinycolor2');
 var InfluenceCircle = require('js/components/lattice/InfluenceCircle');
-var latticeRefreshAlgorithm = require('js/state/lattice/latticeRefreshAlgorithm');
+var LatticePixel = require('js/components/lattice/LatticePixel');
 
-const {WIDTH_PX, HEIGHT_PX, CELL_SIZE, PIXEL_REFRESH_RATE} = require('js/parameters/lattice/LatticeConstants');
+const {WIDTH_PX, HEIGHT_PX} = require('js/parameters/lattice/LatticeConstants');
 
 const SHOW_INFLUENCES = false;
-
-var gray = tinycolor('#909090');
-
-var LatticePixel = React.createClass({
-    componentDidMount: function() {
-        this._refreshOffset = latticeRefreshAlgorithm(this.props.row, this.props.col);
-        setInterval(this._update, this._refreshOffset);
-    },
-    getInitialState: function() {
-        return {
-            color: gray,
-            size: 8,
-            rotation: 0,
-        };
-    },
-    _update: function() {
-        // whether to oscillate (for diamonds/sectors)
-        // setTimeout(this._update, PIXEL_REFRESH_RATE * 2 - this._refreshOffset * 2);
-        this._refreshOffset = PIXEL_REFRESH_RATE - this._refreshOffset;
-        var state = _.clone(this.state);
-        _.each(this.props.influences, influence => influence.mix(state, this.props.row, this.props.col));
-        this.setState(state);
-    },
-    render: function() {
-        var rotation = Math.floor(this.state.rotation);
-        var x = this.props.col * CELL_SIZE + CELL_SIZE / 2;
-        var y = this.props.row * CELL_SIZE + CELL_SIZE / 2;
-        var transform = `translate(${x} ${y}) rotate(${rotation})`;
-        var fill = this.state.color.toHexString(true);
-        var pixelOffset = -this.state.size / 2;
-        return (
-            <rect x={pixelOffset} y={pixelOffset} width={this.state.size} height={this.state.size} fill={fill} transform={transform} />
-        );
-    },
-});
 
 var LatticeGrid = React.createClass({
     render: function() {
---
+++ js/components/lattice/LatticePixel.js
@@ -0,0 +1,43 @@
+var _ = require('underscore');
+var React = require('react');
+var tinycolor = require('tinycolor2');
+var latticeRefreshAlgorithm = require('js/state/lattice/latticeRefreshAlgorithm');
+
+const {CELL_SIZE, PIXEL_REFRESH_RATE} = require('js/parameters/lattice/LatticeConstants');
+
+var gray = tinycolor('#909090');
+
+var LatticePixel = React.createClass({
+    componentDidMount: function() {
+        this._refreshOffset = latticeRefreshAlgorithm(this.props.row, this.props.col);
+        setInterval(this._update, this._refreshOffset);
+    },
+    getInitialState: function() {
+        return {
+            color: gray,
+            size: 8,
+            rotation: 0,
+        };
+    },
+    _update: function() {
+        // whether to oscillate (for diamonds/sectors)
+        // setTimeout(this._update, PIXEL_REFRESH_RATE * 2 - this._refreshOffset * 2);
+        this._refreshOffset = PIXEL_REFRESH_RATE - this._refreshOffset;
+        var state = _.clone(this.state);
+        _.each(this.props.influences, influence => influence.mix(state, this.props.row, this.props.col));
+        this.setState(state);
+    },
+    render: function() {
+        var rotation = Math.floor(this.state.rotation);
+        var x = this.props.col * CELL_SIZE + CELL_SIZE / 2;
+        var y = this.props.row * CELL_SIZE + CELL_SIZE / 2;
+        var transform = `translate(${x} ${y}) rotate(${rotation})`;
+        var fill = this.state.color.toHexString(true);
+        var pixelOffset = -this.state.size / 2;
+        return (
+            <rect x={pixelOffset} y={pixelOffset} width={this.state.size} height={this.state.size} fill={fill} transform={transform} />
+        );
+    },
+});
+
+module.exports = LatticePixel;
