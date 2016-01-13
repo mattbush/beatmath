73b8b0c3bd17aece356de3347b58b294d2c1d125
mattbush <matt.s.bush@gmail.com>
2016-01-13 01:18:54 -0800

    move stuff out of lattice.js

--- js/components/lattice/LatticeGrid.js
+++ js/components/lattice/LatticeGrid.js
@@ -3,16 +3,33 @@ var React = require('react');
 var InfluenceCircle = require('js/components/lattice/InfluenceCircle');
 var LatticePixel = require('js/components/lattice/LatticePixel');
 
-const {WIDTH_PX, HEIGHT_PX} = require('js/parameters/lattice/LatticeConstants');
+const {WIDTH_PX, HEIGHT_PX, NUM_COLS, NUM_ROWS, MAX_SIZE} = require('js/parameters/lattice/LatticeConstants');
+
+var tinycolor = require('tinycolor2');
+var {ColorInfluence, RotationInfluence, SizeInfluence} = require('js/state/lattice/Influence');
 
 const SHOW_INFLUENCES = false;
+
+var influences = [
+    new ColorInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: tinycolor('#800'), index: 0}),
+    new ColorInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: tinycolor('#080'), index: 1}),
+    new ColorInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: tinycolor('#008'), index: 2}),
+
+    new SizeInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
+    new SizeInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
+    new SizeInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
+
+    new RotationInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: 0}),
+    new RotationInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: 0}),
+    new RotationInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: 0}),
+];
 
 var LatticeGrid = React.createClass({
     render: function() {
         const children = [];
-        for (let row = 0; row < this.props.numRows; row++) {
-            for (let col = 0; col < this.props.numCols; col++) {
-                children.push(<LatticePixel influences={this.props.influences} row={row} col={col} key={row + '|' + col} />);
+        for (let row = 0; row < NUM_ROWS; row++) {
+            for (let col = 0; col < NUM_COLS; col++) {
+                children.push(<LatticePixel influences={influences} row={row} col={col} key={row + '|' + col} />);
             }
         }
 
@@ -22,7 +39,7 @@ var LatticeGrid = React.createClass({
                     {children}
                 </g>
                 {SHOW_INFLUENCES && <g>
-                    {_.map(this.props.influences, (influence, index) =>
+                    {_.map(influences, (influence, index) =>
                         <InfluenceCircle influence={influence} key={index} />
                     )}
                 </g>}
--- js/lattice.js
+++ js/lattice.js
@@ -1,29 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var tinycolor = require('tinycolor2');
 var LatticeGrid = require('js/components/lattice/LatticeGrid');
-
-const {NUM_COLS, NUM_ROWS, MAX_SIZE} = require('js/parameters/lattice/LatticeConstants');
-const {ColorInfluence, RotationInfluence, SizeInfluence} = require('js/state/lattice/Influence');
-
-var influences = [
-    new ColorInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: tinycolor('#800'), index: 0}),
-    new ColorInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: tinycolor('#080'), index: 1}),
-    new ColorInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: tinycolor('#008'), index: 2}),
-
-    new SizeInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
-    new SizeInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
-    new SizeInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: MAX_SIZE * 0.5}),
-
-    new RotationInfluence({startCol: 0.2 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: 0}),
-    new RotationInfluence({startCol: 0.8 * NUM_COLS, startRow: 0.2 * NUM_ROWS, startValue: 0}),
-    new RotationInfluence({startCol: 0.5 * NUM_COLS, startRow: 0.8 * NUM_ROWS, startValue: 0}),
-];
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
       <div className="main">
-        <LatticeGrid numRows={NUM_ROWS} numCols={NUM_COLS} influences={influences} />
+        <LatticeGrid />
       </div>,
       document.getElementById('start')
     );
