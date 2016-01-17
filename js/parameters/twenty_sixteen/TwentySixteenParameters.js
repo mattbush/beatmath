ad3751753b0d95d54813a6a45e9082f3f9fa1506
mattbush <matt.s.bush@gmail.com>
2016-01-17 14:39:43 -0800

    shift functions too!

--- js/parameters/twenty_sixteen/TwentySixteenParameters.js
+++ js/parameters/twenty_sixteen/TwentySixteenParameters.js
@@ -20,8 +20,8 @@ class TwentySixteenParameters {
 
         mixboard.addButtonListener(mixboardButton.L_LOOP_MANUAL, this._incrementIndicesDown.bind(this));
         mixboard.addButtonListener(mixboardButton.L_LOOP_IN, this._incrementIndicesUp.bind(this));
-        // mixboard.addButtonListener(mixboardButton.L_LOOP_OUT, this._incrementIndices.bind(this, -1));
-        // mixboard.addButtonListener(mixboardButton.L_LOOP_RELOOP, this._incrementIndices.bind(this, 1));
+        mixboard.addButtonListener(mixboardButton.L_LOOP_OUT, this._shiftIndicesDown.bind(this));
+        mixboard.addButtonListener(mixboardButton.L_LOOP_RELOOP, this._shiftIndicesUp.bind(this));
 
         this.goldIndexMappings = _.times(NUM_GOLD, index => new IndexMappingParameter({start: index}));
         this.blueIndexMappings = _.times(NUM_BLUE, index => new IndexMappingParameter({start: index}));
@@ -33,6 +33,16 @@ class TwentySixteenParameters {
     _incrementIndicesDown() {
         _.map(this.goldIndexMappings, mapping => mapping.mapValue(incrementGoldDown));
         _.map(this.blueIndexMappings, mapping => mapping.mapValue(incrementBlueDown));
+    }
+    _shiftIndicesUp() {
+        var arrangement = ARRANGEMENTS[this.arrangementIndex.getValue()];
+        _.map(this.goldIndexMappings, mapping => mapping.mapValue(arrangement.shiftGoldUp));
+        _.map(this.blueIndexMappings, mapping => mapping.mapValue(arrangement.shiftBlueUp));
+    }
+    _shiftIndicesDown() {
+        var arrangement = ARRANGEMENTS[this.arrangementIndex.getValue()];
+        _.map(this.goldIndexMappings, mapping => mapping.mapValue(arrangement.shiftGoldDown));
+        _.map(this.blueIndexMappings, mapping => mapping.mapValue(arrangement.shiftBlueDown));
     }
 }
 
