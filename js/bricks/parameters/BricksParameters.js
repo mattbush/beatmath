13fd4ec8bb4acbf75339cdce19b062b9700e3953
Matt Bush <matt.s.bush@gmail.com>
2016-03-18 19:47:46 -0700

    declarify BricksParameters

--- js/bricks/parameters/BricksParameters.js
+++ js/bricks/parameters/BricksParameters.js
@@ -4,21 +4,22 @@ var {mixboardWheel, mixboardKnob} = require('js/core/inputs/MixboardConstants');
 const PieceParameters = require('js/core/parameters/PieceParameters');
 
 class BricksParameters extends PieceParameters {
-    constructor() {
-        super(...arguments);
-
-        this.motionAngle = new MovingAngleParameter({
-            max: 1,
-            variance: 0.1,
-            start: 0,
-        });
-        this.motionAngle.listenToWheel(this._mixboard, mixboardWheel.BROWSE);
-
-        this.brickHomogeneity = new LinearParameter({
-            range: [-5, 5],
-            start: -0.4,
-        });
-        this.brickHomogeneity.listenToFader(this._mixboard, mixboardKnob.CUE_GAIN);
+    _declareParameters() {
+        return {
+            motionAngle: {
+                type: MovingAngleParameter,
+                max: 1,
+                variance: 0.1,
+                start: 0,
+                listenToWheel: mixboardWheel.BROWSE,
+            },
+            brickHomogeneity: {
+                type: LinearParameter,
+                range: [-5, 5],
+                start: -0.4,
+                listenToFader: mixboardKnob.CUE_GAIN,
+            },
+        };
     }
 }
 
