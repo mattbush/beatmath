bbaeaa26c53a191186278aa9926b9308fbdc835f
Matt Bush <matt.s.bush@gmail.com>
2016-10-13 01:12:22 -0700

    wallow away

--- js/wallow/WallowHexGrid.js
+++ js/wallow/WallowHexGrid.js
@@ -53,16 +53,43 @@ const x8 = {color: '#802020', points: '3,5 3,3 0,4 0,6'};
 const x9 = {color: '#802020', points: '6,2 6,0 3,1 3,3'};
 const x0 = {color: '#802020', points: '3,3 3,1 0,2 0,4'};
 
-const s1 = {color: '#ff0000', points: '3,4 6,4 6,2'};
+const h1 = {color: '#00aaaa', points: '6,8 12,6 6,4'};
+const h3 = {color: '#00aaaa', points: '12,6 12,2 6,4'};
+const h5 = {color: '#00aaaa', points: '12,2 6,0 6,4'};
+const h7 = {color: '#00aaaa', points: '6,0 0,2 6,4'};
+const h9 = {color: '#00aaaa', points: '0,2 0,6 6,4'};
+const h11 = {color: '#00aaaa', points: '0,6 6,8 6,4'};
+
+const h2 = {color: '#00bb33', points: '6,8 12,6 12,2 6,4'};
+const h6 = {color: '#00bb33', points: '12,2 6,0 0,2 6,4'};
+const h10 = {color: '#00bb33', points: '0,2 0,6 6,8 6,4'};
+const h4 = {color: '#00bb33', points: '12,6 12,2 6,0 6,4'};
+const h8 = {color: '#00bb33', points: '6,0 0,2 0,6 6,4'};
+const h12 = {color: '#00bb33', points: '0,6 6,8 12,6 6,4'};
+
+const a1 = {color: '#550077', points: '6,8 12,2 0,2'};
+const a5 = {color: '#770099', points: '6,6 9,3 3,3'};
+const a3 = {color: '#550077', points: '6,0 0,6 12,6'};
+const a7 = {color: '#770099', points: '6,2 3,5 9,5'};
+
+const a2 = {color: '#770099', points: '12,6 9,3 6,6'};
+const a6 = {color: '#770099', points: '6,0 3,3 9,3'};
+const a10 = {color: '#770099', points: '0,6 6,6 3,3'};
+
+const a4 = {color: '#770099', points: '12,2 6,2 9,5'};
+const a8 = {color: '#770099', points: '0,2 3,5 6,2'};
+const a12 = {color: '#770099', points: '6,8 9,5 3,5'};
+
+const c1 = {color: '#666666', points: '6,6 9,5 9,3 6,2 3,3 3,5'};
 
 const hexGrid = [
-    [[], [], [], [], [], [], [], [x0, y0, z1], [x0, y0, z1], [x0, y0, z1], [x1, x0, y1, y0, z1, z0], [x1, x0, y1, y0, z1, z0, z4, z5, z6, z7], [x1, y6, x5, y1, x0, y5, x6, y0, z1, z5, z6, z0], [x1, x2, x9, x0, y1, y3, y8, y0], [], [x1, x0, y1, y0, z1, z0]],
+    [[], [], [a3], [a3, a5], [h2, h6, h10, a3, a5], [h2, h6, h10, a5], [h2, h6, h10, a5], [x0, y0, z1, a5], [x0, y0, z1, c1, a5], [x0, y0, z1, c1], [x1, x0, y1, y0, z1, z0, c1], [x1, x0, y1, y0, z1, z0, z4, z5, z6, z7], [x1, y6, x5, y1, x0, y5, x6, y0, z1, z5, z6, z0], [x1, x2, x9, x0, y1, y3, y8, y0], [], [x1, x0, y1, y0, z1, z0]],
 
-    [/**/[], [], [], [], [], [], [], [], [], [], [z1, z2, z3, x8, x9, x0, y8, y9, y0], [x1, x2, y3, y1, x0, x9, y8, y0, z4, z5, z6, z7], [x1, x0, y1, y0, z4, z5, z6, z7], [x1, x0, y1, y0], []],
+    [/**/[], [], [a3], [a3, a5], [h4, h8, h12, a5], [h4, h8, h12, a5], [h1, h3, h5, h7, h9, h11, a5], [h1, h3, h5, h7, h9, h11, c1, a5], [h1, h3, h5, h7, h9, h11, c1], [h4, h8, h12, c1], [z1, z2, z3, x8, x9, x0, y8, y9, y0, c1], [x1, x2, y3, y1, x0, x9, y8, y0, z4, z5, z6, z7], [x1, x0, y1, y0, z4, z5, z6, z7], [x1, x0, y1, y0], []],
 
-    [[], [], [], [], [], [], [], [], [], [], [x4, y4, z7], [z1, z2, z3, x8, y4, x4, y9, z7, x0, x9, y8, y0], [z1, z2, z3, x8, y4, x4, y9, x0, y5, x6, y0, z0], [x1, x0, y1, y0, z5, z6], [x0, y5, x6, y0, z1, z0], [x8, x0, z0, y9, y0]],
+    [[], [], [], [a3], [a3, a5], [a3, a5], [h4, h8, h12, a1, a5], [a1, a5], [a1, c1], [h4, h8, h12, a1, c1], [h4, h8, h12, x4, y4, z7], [z1, z2, z3, x8, y4, x4, y9, z7, x0, x9, y8, y0], [z1, z2, z3, x8, y4, x4, y9, x0, y5, x6, y0, z0], [x1, x0, y1, y0, z5, z6], [x0, y5, x6, y0, z1, z0], [x8, x0, z0, y9, y0]],
 
-    [/**/[], [], [], [], [], [], [], [], [], [z4, x7, y7], [z4, x7, y7], [z1, z2, z3, z4, x8, x7, y7, y9, x0, x9, y8, y0], [x0, x9, y8, y0, z1], [x0, y0, z1], [x1, x0, y1, y0, z1, z0]],
+    [/**/[], [h1, h3, h5, h7, h9, h11], [h1, h3, h5, h7, h9, h11], [h1, h3, h5, h7, h9, h11], [h1, h3, h5, h7, h9, h11, a3, a5], [h1, h3, h5, h7, h9, h11], [h1, h3, h5, h7, h9, h11], [a1], [a1], [h4, h8, h12, a1, z4, x7, y7], [h4, h8, h12, z4, x7, y7], [z1, z2, z3, z4, x8, x7, y7, y9, x0, x9, y8, y0], [h4, h8, h12, x0, x9, y8, y0, z1], [h4, h8, h12, x0, y0, z1], [h4, h8, h12, x1, x0, y1, y0, z1, z0]],
 
     [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
 
