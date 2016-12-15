250edc239b3de251c5fdf44afaeb4db18a46e89b
Matt Bush <matt.s.bush@gmail.com>
2016-12-14 22:49:43 -0800

    remove _getColorByAnchor

--- js/kale/components/KaleCell.js
+++ js/kale/components/KaleCell.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const React = require('react');
 const ParameterBindingsMixin = require('js/core/components/ParameterBindingsMixin');
 const KaleSubject = require('js/kale/components/KaleSubject');
-const {dist, lerp, posMod, xyRotatedAroundOriginWithAngle} = require('js/core/utils/math');
+const {dist, lerp, xyRotatedAroundOriginWithAngle} = require('js/core/utils/math');
 const tinycolor = require('tinycolor2');
 const {clipPathXCenters, clipPathYCenters} = require('js/kale/components/KaleClipPaths');
 
@@ -29,8 +29,7 @@ const KaleCell = React.createClass({
         return Math.round(dist(this.props.logicalX, this.props.logicalY)) * 20 % 50;
     },
     _getColorByShifting: function(x, y) {
-        const colorsByCoords = this.context.kaleParameters.colorsByCoords;
-        const color = tinycolor(colorsByCoords['0,0'].getValue().toHexString()); // clone
+        const color = tinycolor(this.context.kaleParameters.baseColor.getValue().toHexString()); // clone
         const colColorShift = this.context.kaleParameters.colColorShift.getValue();
         const rowColorShift = this.context.kaleParameters.rowColorShift.getValue();
         const colorShift = colColorShift * x + rowColorShift * y;
@@ -41,67 +40,6 @@ const KaleCell = React.createClass({
     },
     _getColor: function(x, y) {
         return this._getColorByShifting(x, y);
-    },
-    _getColorByAnchor: function() {
-        const x = this.props.logicalX;
-        const y = this.props.logicalY;
-        let xMod6 = posMod(x, 6);
-        const xDiv6 = (x - xMod6) / 6;
-        let yMod6 = posMod(y, 6);
-        const yDiv6 = (y - yMod6) / 6;
-
-        let lowerLeft = [xDiv6 * 6, yDiv6 * 6].join(',');
-        let lowerRight = [xDiv6 * 6 + 6, yDiv6 * 6].join(',');
-        const mid = [xDiv6 * 6 + 3, yDiv6 * 6 + 3].join(',');
-        let upperLeft = [xDiv6 * 6, yDiv6 * 6 + 6].join(',');
-        let upperRight = [xDiv6 * 6 + 6, yDiv6 * 6 + 6].join(',');
-
-        if (xMod6 > 3) {
-            xMod6 = 6 - xMod6;
-            [lowerLeft, lowerRight, upperLeft, upperRight] = [lowerRight, lowerLeft, upperRight, upperLeft];
-        }
-        if (yMod6 > 3) {
-            yMod6 = 6 - yMod6;
-            [lowerLeft, lowerRight, upperLeft, upperRight] = [upperLeft, upperRight, lowerLeft, lowerRight];
-        }
-        switch (`${xMod6},${yMod6}`) {
-            case '0,0':
-                return this._mixColors(lowerLeft);
-            case '0,2':
-                return this._mixColors(lowerLeft, lowerLeft, lowerRight);
-            case '1,1':
-                return this._mixColors(lowerLeft, lowerLeft, mid);
-            case '1,3':
-                return this._mixColors(lowerLeft, mid, lowerRight);
-            case '2,0':
-                return this._mixColors(lowerLeft, lowerLeft, upperLeft);
-            case '2,2':
-                return this._mixColors(lowerLeft, mid, mid);
-            case '3,1':
-                return this._mixColors(lowerLeft, mid, upperLeft);
-            case '3,3':
-                return this._mixColors(mid);
-            default:
-                throw new Error('color combo shouldnt exist');
-        }
-    },
-    _mixColors: function(...coords) {
-        const colorsByCoords = this.context.kaleParameters.colorsByCoords;
-        if (coords.length === 1) {
-            return colorsByCoords[coords[0]].getValue();
-        } else {
-            const initialMix = tinycolor.mix(
-                colorsByCoords[coords[0]].getValue(),
-                colorsByCoords[coords[1]].getValue(),
-                50,
-            );
-
-            return tinycolor.mix(
-                initialMix,
-                colorsByCoords[coords[2]].getValue(),
-                33,
-            );
-        }
     },
     render: function() {
         const isInfinite = this.getParameterValue('isInfinite');
