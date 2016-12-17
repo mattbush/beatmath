8306510a37554d2ededdbb2fdb740cb3c69baeba
Matt Bush <matt.s.bush@gmail.com>
2016-12-16 23:06:24 -0800

    Snowstorm

---
+++ js/snowstorm/components/SnowstormContainer.js
@@ -0,0 +1,32 @@
+const React = require('react');
+const SnowstormParameters = require('js/snowstorm/parameters/SnowstormParameters');
+const BeatmathFrame = require('js/core/components/BeatmathFrame');
+
+const SnowstormContainer = React.createClass({
+    childContextTypes: {
+        snowstormParameters: React.PropTypes.object,
+    },
+    contextTypes: {
+        mixboard: React.PropTypes.object,
+        beatmathParameters: React.PropTypes.object,
+    },
+    getChildContext: function() {
+        return {
+            snowstormParameters: this.state.snowstormParameters,
+        };
+    },
+    getInitialState: function() {
+        return {
+            snowstormParameters: new SnowstormParameters(this.context.mixboard, this.context.beatmathParameters),
+        };
+    },
+    render: function() {
+        return (
+            <BeatmathFrame>
+                Hello world!
+            </BeatmathFrame>
+        );
+    },
+});
+
+module.exports = SnowstormContainer;
---
+++ js/snowstorm/parameters/SnowstormParameters.js
@@ -0,0 +1,9 @@
+// const {LinearParameter} = require('js/core/parameters/Parameter');
+// const {MixtrackKnobs} = require('js/core/inputs/MixtrackConstants');
+const PieceParameters = require('js/core/parameters/PieceParameters');
+
+class SnowstormParameters extends PieceParameters {
+
+}
+
+module.exports = SnowstormParameters;
