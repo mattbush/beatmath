ed6e0fdc3f710dad830ec4174bda99adf5ab4d6a
Matt Bush <matt.s.bush@gmail.com>
2016-12-16 23:37:00 -0800

    nothing like a good 6-way

--- js/snowstorm/components/Snowflake.js
+++ js/snowstorm/components/Snowflake.js
@@ -1,4 +1,4 @@
-// const _ = require('lodash');
+const _ = require('lodash');
 const React = require('react');
 const ParameterBindingsMixin = require('js/core/components/ParameterBindingsMixin');
 // const {lerp, clamp} = require('js/core/utils/math');
@@ -16,10 +16,27 @@ const Snowflake = React.createClass({
     },
     render: function() {
         // const snowstormParameters = this.context.snowstormParameters;
+        const points = [];
+        const HYPOTENUSE = 2 * (3 ** -0.5);
+        const TANGENT = 3 ** -0.5;
+        const length1 = 1;
+        const width1 = 0.1;
+
+        _.times(6, i => {
+            const angle = Math.PI * i / 3;
+            const cos0 = Math.cos(angle);
+            const sin0 = Math.sin(angle);
+            const addPoint = (x, y) => points.push([x * cos0 - y * sin0, y * cos0 + x * sin0]);
+
+            addPoint(width1, width1 * TANGENT + width1 * HYPOTENUSE);
+            addPoint(width1, length1 + width1 * TANGENT);
+            addPoint(0, length1 + width1 * HYPOTENUSE);
+            addPoint(-width1, length1 + width1 * TANGENT);
+        });
 
         return (
             <g style={{transform: 'scale(200)'}}>
-                <polygon points="1,1 0,0 1,-1" fill="#0ff" />
+                <polygon points={points.map(p => p.join(',')).join(' ')} fill="#0ff" />
             </g>
         );
     },
