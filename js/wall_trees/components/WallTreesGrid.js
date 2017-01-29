c9d5132f40688cb2213d40e4a876233af7ac611a
Matt Bush <matt.s.bush@gmail.com>
2017-01-28 20:24:09 -0800

    give trees color

--- js/wall_trees/components/WallTreesGrid.js
+++ js/wall_trees/components/WallTreesGrid.js
@@ -4,8 +4,13 @@ const React = require('react');
 
 const hexGrid = require('js/wallow/WallowHexGrid');
 const WallTreesHex = require('js/wall_trees/components/WallTreesHex');
+const {ENABLE_HUE} = require('js/lattice/parameters/LatticeConstants');
+const updateHue = require('js/core/outputs/updateHue');
 
 const WallTreesGrid = React.createClass({
+    contextTypes: {
+        wallTreesParameters: React.PropTypes.object,
+    },
     render: function() {
         const componentGrid = _.map(hexGrid, (hexes, row) => {
             return _.map(hexes, (hex, column) => {
@@ -16,6 +21,14 @@ const WallTreesGrid = React.createClass({
                 return <WallTreesHex row={row} column={column} />;
             });
         });
+
+        if (ENABLE_HUE) {
+            const hueInOrder = [8, 1, 2, 7, 6];
+            hueInOrder.forEach((lightNumber, index) => {
+                const color = this.context.wallTreesParameters.getColorForColumnAndRow(index * 2, 0);
+                updateHue(lightNumber, color, {briCoeff: 0.4});
+            });
+        }
 
         return (
             <g style={{transform: `scale(76) translate(${-(_.size(hexGrid[0]) - 1) / 2}px, ${-_.size(hexGrid) / 2}px)`}}>
