358d5f39e30a9879e7882d2b7953879872509e30
Matt Bush <matt.s.bush@gmail.com>
2017-03-23 00:58:16 -0700

    update stopwatch every N

--- js/stopwatch/components/StopwatchParticle.js
+++ js/stopwatch/components/StopwatchParticle.js
@@ -4,10 +4,17 @@ const {clamp, modAndShiftToHalf, lerp, logerp} = require('js/core/utils/math');
 
 const TWOPI = 2 * Math.PI;
 
+const UPDATE_EVERY_N = 4;
+
 const StopwatchParticle = React.createClass({
     contextTypes: {
         beatmathParameters: React.PropTypes.object,
         stopwatchParameters: React.PropTypes.object,
+    },
+    shouldComponentUpdate() {
+        const currentTick = this.context.beatmathParameters.tempo.getNumTicks();
+        const trailPosition = currentTick - this.props.tick;
+        return trailPosition % UPDATE_EVERY_N === 1;
     },
     render() {
         if (this.props.visibleIndex === undefined) {
@@ -34,6 +41,7 @@ const StopwatchParticle = React.createClass({
         const polarGridAmount = clamp(stopwatchParameters.polarGridAmount.getValue(), 0, 1);
         let polarO;
         let cartesianO;
+        let timing;
 
         if (polarGridAmount > 0) {
             const scale = 32 * GROWTH_RATIO ** trailPosition; // TODO cache this
@@ -50,11 +58,14 @@ const StopwatchParticle = React.createClass({
                 rotation: TWOPI / 4 + rotationRad,
 
                 px1: GROWTH_RATIO * Math.cos(-dTheta - widthRad / 2), py1: GROWTH_RATIO * Math.sin(-dTheta - widthRad / 2),
-                px2: GROWTH_RATIO * Math.cos(-dTheta + widthRad / 2), py2: GROWTH_RATIO * Math.sin(-dTheta + widthRad / 2),
-                px3: Math.cos(widthRad / 2), py3: Math.sin(widthRad / 2),
-                px4: Math.cos(-widthRad / 2), py4: Math.sin(-widthRad / 2),
+                // px2: GROWTH_RATIO * (1 + widthRad / 2) * Math.cos(-dTheta), py2: GROWTH_RATIO * Math.sin(-dTheta),
+                px3: GROWTH_RATIO * Math.cos(-dTheta + widthRad / 2), py3: GROWTH_RATIO * Math.sin(-dTheta + widthRad / 2),
+                px4: Math.cos(widthRad / 2), py4: Math.sin(widthRad / 2),
+                // px5: 1 / (1 + widthRad / 2), py5: 0,
+                px6: Math.cos(-widthRad / 2), py6: Math.sin(-widthRad / 2),
             };
 
+            timing = 'cubic-bezier(0.4, 0.25, 0.75, 0.6)';
         }
 
         if (polarGridAmount < 1) {
@@ -77,10 +88,14 @@ const StopwatchParticle = React.createClass({
                 rotation: 0,
 
                 px1: -dx - w / 2, py1: -h / 2,
-                px2: -dx + w / 2, py2: -h / 2,
-                px3: w / 2, py3: h / 2,
-                px4: -w / 2, py4: h / 2,
+                // px2: -dx, py2: -h / 2 - w / 2,
+                px3: -dx + w / 2, py3: -h / 2,
+                px4: w / 2, py4: h / 2,
+                // px5: 0, py5: h / 2 + w / 2,
+                px6: -w / 2, py6: h / 2,
             };
+
+            timing = 'linear';
         }
 
         const o =
@@ -91,23 +106,25 @@ const StopwatchParticle = React.createClass({
 
         // TODO: instead of passing trailPosition could pass this.props.tick or something else
         const fill = stopwatchParameters.getColorForTrailAndTick(this.props.visibleIndex, trailPosition);
-        const delay = beatmathParameters.tempo.getPeriod() * stopwatchParameters.delayCoefficient.getValue();
+        const delay = beatmathParameters.tempo.getPeriod() * stopwatchParameters.delayCoefficient.getValue() * UPDATE_EVERY_N;
 
         const style = {
             transform: `scale(${o.scale}) rotate(${o.rotation}rad) translate(${o.x}px,${o.y}px)`,
-            transition: `transform ${delay}ms linear`,
+            transition: `transform ${delay}ms ${timing}`,
         };
 
         // stop logspew, TODO look into
-        if ([o.px1, o.py1, o.px2, o.py2, o.px3, o.py3, o.px4, o.py4].some(isNaN)) {
+        if ([o.px1, o.py1, o.px3, o.py3, o.px4, o.py4, o.px6, o.py6].some(isNaN)) {
             return null;
         }
 
         const points = [
             [o.px1, o.py1],
-            [o.px2, o.py2],
+            // [o.px2, o.py2],
             [o.px3, o.py3],
             [o.px4, o.py4],
+            // [o.px5, o.py5],
+            [o.px6, o.py6],
         ].map(p => p.join(',')).join(' ');
 
         return (
