424a211b68b04cff599f4536053d82632db3ee03
Matt Bush <matt.s.bush@gmail.com>
2017-10-21 18:54:58 -0700

    simplify WallCircuitParameters

--- js/wall_circuit/parameters/WallCircuitParameters.js
+++ js/wall_circuit/parameters/WallCircuitParameters.js
@@ -1,30 +1,19 @@
 const _ = require('lodash');
-const {MovingLinearParameter, LogarithmicParameter, HoldButtonParameter} = require('js/core/parameters/Parameter');
+const {MovingColorParameter, LogarithmicParameter} = require('js/core/parameters/Parameter'); // MovingLinearParameter
 const {MixtrackButtons} = require('js/core/inputs/MixtrackConstants');
 const tinycolor = require('tinycolor2');
-const {posMod, posModAndBendToLowerHalf, lerp} = require('js/core/utils/math');
+const {posMod, lerp} = require('js/core/utils/math'); // posModAndBendToLowerHalf
 const PieceParameters = require('js/core/parameters/PieceParameters');
 const {arclerp} = require('js/core/utils/math');
-const {LaunchpadButtons} = require('js/core/inputs/LaunchpadConstants');
-const P = require('js/core/parameters/P');
 const {ENABLE_HUE} = require('js/lattice/parameters/LatticeConstants');
 const updateHue = require('js/core/outputs/updateHue');
 const {NUM_LIGHTS} = require('js/hue_constants');
-
-const NUM_TREES = 15;
-const NUM_LEVELS = 8;
-
-const PI_TIMES_2 = Math.PI * 2;
-
-const WHITE = tinycolor('#fff');
-const BLACK = tinycolor('#000');
 
 class WallCircuitParameters extends PieceParameters {
     constructor(mixboard, beatmathParameters) {
         super(...arguments);
 
         this._riseNumTicks = 0;
-        this._sineNumTicks = 0;
 
         beatmathParameters.tempo.addListener(this._incrementNumTicks.bind(this));
 
@@ -36,7 +25,13 @@ class WallCircuitParameters extends PieceParameters {
     }
     _declareParameters() {
         return {
-            ...P.BaseColor(),
+            baseColor: {
+                type: MovingColorParameter,
+                start: tinycolor('#5ff'),
+                max: 5,
+                variance: 1,
+                autoupdate: 1000,
+            },
             periodTicks: {type: LogarithmicParameter,
                 range: [2, 64],
                 start: 16,
@@ -44,118 +39,19 @@ class WallCircuitParameters extends PieceParameters {
                 listenToDecrementAndIncrementMixtrackButtons: [MixtrackButtons.L_LOOP_OUT, MixtrackButtons.L_LOOP_RELOOP],
                 monitorName: 'Period Ticks',
             },
-            ...P.ColumnColorShift({range: 45}),
-            ...P.RowColorShift({range: 45}),
             // ...P.CustomPercent({name: 'trailPercent', start: 0.5, inputPosition: [2, 2]}),
             // ...P.CustomPercent({name: 'revTrailPercent', start: 0, inputPosition: [1, 2]}),
-            staggerAmount: {
-                type: MovingLinearParameter,
-                range: [-8, 8],
-                start: 0,
-                monitorName: 'Stagger Amount',
-                listenToDecrementAndIncrementLaunchpadButtons: 2,
-                listenToDecrementAndIncrementMixtrackButtons: [MixtrackButtons.L_PITCH_BEND_MINUS, MixtrackButtons.L_PITCH_BEND_PLUS],
-                variance: 0.5,
-                autoupdateEveryNBeats: 4,
-                autoupdateOnCue: true,
-                canSmoothUpdate: true,
-            },
-            ...P.CustomToggle({name: 'mirrorStagger', button: 1}),
-            ...P.CustomToggle({name: 'roundStagger', button: 0, start: true}),
-            riseDirection: {
-                type: MovingLinearParameter,
-                range: [-1, 1],
-                start: 1,
-                monitorName: 'Rise Dir',
-                listenToLaunchpadKnob: [2, 3],
-                variance: 0.04,
-                autoupdateEveryNBeats: 4,
-                autoupdateOnCue: true,
-            },
-            sineDirection: {
-                type: MovingLinearParameter,
-                range: [-1, 1],
-                start: 1,
-                monitorName: 'Sine Dir',
-                listenToLaunchpadKnob: [2, 4],
-                variance: 0.04,
-                autoupdateEveryNBeats: 4,
-                autoupdateOnCue: true,
-                canSmoothUpdate: true,
-            },
-            sineAmplitude: {
-                type: MovingLinearParameter,
-                range: [0, 1],
-                start: 0,
-                monitorName: 'Sine Amp',
-                listenToLaunchpadFader: [4, {addButtonStatusLight: true}],
-                variance: 0.01,
-                autoupdateEveryNBeats: 2,
-                autoupdateOnCue: true,
-                canSmoothUpdate: true,
-            },
-            sinePeriodTicks: {type: LogarithmicParameter,
-                range: [4, 64],
-                start: 16,
-                listenToDecrementAndIncrementLaunchpadButtons: 4,
-                monitorName: 'Sine Ticks',
-            },
-            blackout: {
-                type: HoldButtonParameter,
-                start: false,
-                listenToLaunchpadButton: LaunchpadButtons.TRACK_FOCUS[5],
-            },
-            whiteout: {
-                type: HoldButtonParameter,
-                start: false,
-                listenToLaunchpadButton: LaunchpadButtons.TRACK_CONTROL[5],
-            },
         };
     }
     _incrementNumTicks() {
-        this._riseNumTicks += this.riseDirection.getValue();
-        this._sineNumTicks += this.sineDirection.getValue();
-    }
-    _getSineAmount(columnIndex) {
-        const sineAmplitude = this.sineAmplitude.getValue();
-        if (sineAmplitude === 0) {
-            return 0;
-        }
-        const sinePeriodTicks = this.sinePeriodTicks.getValue();
-        const sineWaveAngularOffsetPercent = (this._sineNumTicks + columnIndex) / sinePeriodTicks;
-        return sineAmplitude * NUM_LEVELS * Math.sin(sineWaveAngularOffsetPercent * PI_TIMES_2);
+        this._riseNumTicks += 1;
     }
     _getRowIllumination(columnIndex, rowIndex) {
         const periodTicks = this.periodTicks.getValue();
-        let staggerAmount = this.staggerAmount.getValue();
-        if (this.roundStagger.getValue()) {
-            staggerAmount = Math.round(staggerAmount);
-        }
-        if (staggerAmount !== 0) {
-            if (this.mirrorStagger.getValue()) {
-                columnIndex = posModAndBendToLowerHalf(columnIndex, (NUM_TREES / 2.5));
-            }
-            rowIndex -= (columnIndex - NUM_TREES / 2) * staggerAmount;
-        }
-        const sineAmount = this._getSineAmount(columnIndex - NUM_TREES / 2);
-        return posMod(this._riseNumTicks - rowIndex + sineAmount, periodTicks) / periodTicks;
-    }
-    _getColorShiftPerColumn() {
-        return this.columnColorShift.getValue();
+        return posMod(this._riseNumTicks - rowIndex, periodTicks) / periodTicks;
     }
     getColorForColumnAndRow(column, row) {
-        if (this.whiteout.getValue()) {
-            return WHITE;
-        } else if (this.blackout.getValue()) {
-            return BLACK;
-        }
         const color = tinycolor(this.baseColor.getValue().toHexString()); // clone
-        const colorShiftPerColumn = this._getColorShiftPerColumn();
-        const colorShiftPerRow = this.rowColorShift.getValue();
-        const colorShift = colorShiftPerColumn * column + colorShiftPerRow * row;
-        if (colorShift !== 0) {
-            color.spin(colorShift);
-        }
         const baseRowIllumination = this._getRowIllumination(column, row);
         const revTrailPercent = 0.5; // 1 - this.revTrailPercent.getValue();
         let rowIllumination;
