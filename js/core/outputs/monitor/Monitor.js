2d0096267fc246dc64c88f38c5be61a95291b534
Matt Bush <matt.s.bush@gmail.com>
2017-12-01 22:45:25 -0800

    channel manager

--- js/core/outputs/monitor/Monitor.js
+++ js/core/outputs/monitor/Monitor.js
@@ -2,8 +2,14 @@ const _ = require('lodash');
 const React = require('react');
 const MonitorValue = require('js/core/outputs/monitor/MonitorValue');
 const PieceNameToLoadInput = require('js/core/outputs/monitor/PieceNameToLoadInput');
+const MonitorChannelManager = require('js/core/outputs/monitor/MonitorChannelManager');
 
 const Monitor = React.createClass({
+    getInitialState() {
+        return {
+            isInChannelManager: false,
+        };
+    },
     componentDidMount() {
         this._parsedStorage = {};
         this._throttledForceUpdate = _.throttle(this.forceUpdate.bind(this), 50);
@@ -16,16 +22,33 @@ const Monitor = React.createClass({
         this._parsedStorage[event.key] = JSON.parse(event.newValue);
         this._throttledForceUpdate();
     },
+    _onTabsClick() {
+        this.setState({
+            isInChannelManager: !this.state.isInChannelManager,
+        });
+    },
     render() {
+        const {isInChannelManager} = this.state;
+
         return (
             <div className="main">
-                {_.map(_.pickBy(this._parsedStorage), (value, key) =>
-                    <MonitorValue
-                        key={key}
-                        value={value}
-                    />
-                )}
-                <PieceNameToLoadInput />
+                <div onClick={this._onTabsClick}>
+                    <span className={isInChannelManager ? 'tab' : 'tab sel'}>Monitor</span>
+                    <span className={isInChannelManager ? 'tab sel' : 'tab'}>Channel Manager</span>
+                </div>
+                {isInChannelManager ?
+                    <MonitorChannelManager /> :
+                    <div>
+                        {_.map(_.pickBy(this._parsedStorage), (value, key) =>
+                            <MonitorValue
+                                key={key}
+                                value={value}
+                            />
+                        )}
+                        <PieceNameToLoadInput />
+
+                    </div>
+                }
             </div>
         );
     },
---
+++ js/core/outputs/monitor/MonitorChannelManager.js
@@ -0,0 +1,13 @@
+// const _ = require('lodash');
+const React = require('react');
+
+const MonitorChannelManager = React.createClass({
+    render() {
+        return (
+            <div className="hello">
+            </div>
+        );
+    },
+});
+
+module.exports = MonitorChannelManager;
